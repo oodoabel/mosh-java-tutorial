01178c69bc3fe8c85866f596d4593208c78730e1
oodoabel <get2abeloodo@gmail.com>
2025-03-13 03:29:41 +0100

    string

--- Main.java
+++ Main.java
@@ -1,15 +1,8 @@
-import java.awt.*;
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
-
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x=2;
-        System.out.println(point2);
+        String message = "Hello world" + "!!";
+        System.out.println(message.indexOf("Sky"));
+        System.out.println(message.trim());
     }
 }
