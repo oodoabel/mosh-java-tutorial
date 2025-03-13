97c2cf5754a3afc0facba2eda42286145c04b158
oodoabel <get2abeloodo@gmail.com>
2025-03-13 03:19:01 +0100

    reference types

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args){
         byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
 
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = true;
-
-        System.out.println(age);
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x=2;
+        System.out.println(point2);
     }
 }
