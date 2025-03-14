a1310ca0a1866e77483fafc09430857ec8d02712
oodoabel <get2abeloodo@gmail.com>
2025-03-14 03:31:50 +0100

    maths class

--- Main.java
+++ Main.java
@@ -1,17 +1,8 @@
 
 public class Main {
     public static void main(String[] args){
-       //implicit casting
-        //byte > short > int > long > float > double
-       double x = 1.1;
-       int y = (int)x + 2; //explicit casting
-
-        System.out.println(y);
-        String c = "1.1";
-        double yy = Double.parseDouble(c) + 2;
-        System.out.println(yy);
-
-
+       int result = (int) Math.round(Math.random() * 100);
+        System.out.println(result);
 
     }
 }
