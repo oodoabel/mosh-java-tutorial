086c4d93fb8a5d4a4713202dd9d843471e3b47a5
oodoabel <get2abeloodo@gmail.com>
2025-04-22 00:45:07 +0100

    is the number even or odd?

--- Main.java
+++ Main.java
@@ -4,11 +4,15 @@ public class Main{
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Type a sentence");
-        String Sentence = input.nextLine();
+        System.out.println("Enter a whole number");
+        int number = input.nextInt();
+         if (number % 2 == 0){
+            System.out.println("Number is even");
+         }else {
+             System.out.println("Number is ood");
+         }
 
-        int num = Sentence.length();
-        System.out.println("Your sentence has a total of "+num +" words long");
+         input.close();
 
     }
 }
