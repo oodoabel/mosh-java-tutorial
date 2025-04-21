2b53bbe02ef4a41b3da6a39c3fd01f89b286c982
oodoabel <get2abeloodo@gmail.com>
2025-04-22 00:37:45 +0100

    number of word in a sentense task

--- Main.java
+++ Main.java
@@ -4,26 +4,11 @@ public class Main{
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter your first integer");
-        int firstInt = input.nextInt();
+        System.out.println("Type a sentence");
+        String Sentence = input.nextLine();
 
-        System.out.println("Enter your Second integer");
-        int secondInt = input.nextInt();
-
-        int sum = firstInt+secondInt;
-
-        int difference = firstInt-secondInt;
-
-        int product = firstInt*secondInt;
-
-        int quotient = firstInt/secondInt;
-
-        System.out.println("Sum: "+ sum);
-        System.out.println("Difference: "+ difference);
-        System.out.println("Product: "+ product);
-        System.out.println("Quotient: "+ quotient);
-
-
+        int num = Sentence.length();
+        System.out.println("Your sentence has a total of "+num +" words long");
 
     }
 }
