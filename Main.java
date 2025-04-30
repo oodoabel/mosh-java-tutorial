78c6d81d3883bddc68f89e8c0cf7887028dc536b
oodoabel <get2abeloodo@gmail.com>
2025-04-30 04:56:08 +0100

    Calculator in Java in OOP in CPT311

--- Main.java
+++ Main.java
@@ -4,15 +4,32 @@ public class Main{
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter a whole number");
-        int number = input.nextInt();
-         if (number % 2 == 0){
-            System.out.println("Number is even");
-         }else {
-             System.out.println("Number is ood");
-         }
+        System.out.println("Enter Operand 1");
+        double num1 = input.nextDouble();
 
-         input.close();
+        System.out.println("Enter Operator");
+        char operator = input.next().charAt(0);
 
+        System.out.println("Enter Operand 2");
+        double num2 = input.nextDouble();
+
+        double result = 0;
+
+        switch (operator){
+            case '-':
+                result = num1 - num2;
+                break;
+            case'+':
+                result = num1 + num2;
+            case'*':
+                result = num1 * num2;
+            case'/':
+                if (num2 == 0){
+                    System.out.println("Cannot devide by 0");
+                }else{
+                result = num1 + num2;}
+        }
+
+        System.out.println(num1 +" "+operator +" " + num2 +" " + "=" + " " + result);
     }
 }
